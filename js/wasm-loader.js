dfca8c69cfbe8e2ed518ff17354c1c247d787fff
Nuthrash <nuthrash@outlook.com>
2019-05-10 10:52:09 +0800

    Add test/wasm/wasm_test project for WebAssembly

---
+++ js/wasm-loader.js
@@ -0,0 +1,11 @@
+﻿function fetchAndInstantiateWasm (url, imports) {
+    return fetch(url) // url could be your .wasm file
+    .then(res => {
+		if (res.ok)
+			return res.arrayBuffer();
+		throw new Error(`Unable to fetch Web Assembly file ${url}.`);
+		})
+    .then(bytes => WebAssembly.compile(bytes))
+    .then(module => WebAssembly.instantiate(module, imports || {}))
+    .then(instance => instance.exports);
+}
